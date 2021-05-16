b05fbc5879cafe38098b8272491b45a821c60225
wandji20 <wbertrand10@gmail.com>
2021-05-16 12:42:40 +0100

    Setup phaser

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Yooo');
+import './style.css'
+import 'phaser';
+
+// import { SimpleScene } from './scenes/simple-scene';
+let gameScene = new Phaser.Scene('Game')
+let gameConfig = {
+  width: 680,
+  height: 400,
+  scene: gameScene
+};
+
+new Phaser.Game(gameConfig);
